a3e0612fc57347397b2f8d0624cbb0cb86a472ab
Spencer Mathews <smathews0906@gmail.com>
2022-06-14 07:48:58 -0500

    commit tree cae52b6ab5a94aabfa4866b119b5d82c7b946e9c

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Add your code here
+const form ={
+        name:"Spencer",
+        email:"samtha@gmail.com",
+}
+
+function submitData(formData){
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+      });
+}
+
+submitData(form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // console.log(form)
+
+// // function submitData(form){
+// //     // const configurationObject = {
+// //     //     method: "POST",
+// //     //     headers: {
+// //     //       "Content-Type": "application/json",
+// //     //       "Accept": "application/json",
+// //     //     },
+// //     //     body: JSON.stringify(form),
+// //     //   };
+// //     fetch("http://localhost:3000/users", {
+// //         method: "POST",
+// //         headers: {
+// //           "Content-Type": "application/json",
+// //           "Accept": "application/json",
+// //         },
+// //         body: JSON.stringify(form),
+// //       })
+// //       .then(res => res.json())
+// //       .then(data=>console.log(data))
+// // };
